e25a6363dbe2171fa38e6cc018d354148a975404
Laura Goon <goon.laura@gmail.com>
2022-11-27 15:19:22 -0500

    add a simple test file

---
+++ tests/fxn.py
@@ -0,0 +1,20 @@
+def print_sentence(name, num, color=orange):
+    """
+    Prints a sentence given the inputs.
+    Returns: [None]
+    
+    name [str] : name of person
+    num [int] : person's favorite number
+    color [str][optional] : person's favorite color
+    """
+    print("Hi, my name is " + name + ". My favorite number is " + str(num) + " and my favorite color is " + color)
+    
+def sum_two_int(int_a, int_b):
+    """
+    Adds 2 integers and returns the sum.
+    Returns: [int] Sum of the inputs
+    
+    int_a [int] : first integer
+    int_b [int] : second integer
+    """
+    return int_a + int_b
