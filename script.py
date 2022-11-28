577b7e8c110eaf0d2fa90ed2ef03f7596bf8e1f6
Laura Goon <goon.laura@gmail.com>
2022-11-27 19:15:22 -0500

    crude fxn to generate html, incorporate enum usage

--- script.py
+++ script.py
@@ -1,25 +1,23 @@
 import re
 
-NAME = "name"
-PARAM_NAMES = "param_names"
-PARAM_OPTIONAL_VALS = "param_optional_vals"
-DESC = "desc"
-RETURN_TYPE = "return_type"
-RETURN_DESC = "return_desc"
-PARAM_DESC = "param_desc"
+from docgen import generate_fxn_html, generate_init_html, generate_end_html
+from enums import FunctionAttrs
+
 
 def init_fxn_info_dict():
     return {
-        NAME: None,
-        PARAM_NAMES: [],
-        PARAM_OPTIONAL_VALS: {},
-        DESC: "",
-        RETURN_TYPE: None,
-        RETURN_DESC: "",
-        PARAM_DESC: {} # { param : [type, desc] }
+        FunctionAttrs.NAME.value: None,
+        FunctionAttrs.PARAM_NAMES.value: [],
+        FunctionAttrs.PARAM_OPTIONAL_VALS.value: {},
+        FunctionAttrs.DESC.value: "",
+        FunctionAttrs.RETURN_TYPE.value: None,
+        FunctionAttrs.RETURN_DESC.value: "",
+        FunctionAttrs.PARAM_DESC.value: {} # { param : [type, desc] }
     }
 
 def process_file(filename):
+    processed_fxn = []
+    
     with open(filename) as fp:
         line_num = 0
         at_doc_comment = False
@@ -34,22 +32,24 @@ def process_file(filename):
             if stripped_line[0:3] == "def":
                 
                 # clear vars for new fxn info
+                if curr_fxn[FunctionAttrs.NAME.value] is not None:
+                    processed_fxn.append(curr_fxn)
                 curr_fxn = init_fxn_info_dict()
                 
                 line_delimed = re.split(r"\s|\(|\)|,|:", stripped_line)
                 line_delimed = list(filter(None, line_delimed)) # remove empty strings from regex split
                 
-                curr_fxn[NAME] = line_delimed[1]
+                curr_fxn[FunctionAttrs.NAME.value] = line_delimed[1]
                 
                 param_declarations = line_delimed[2:]
                 for param in param_declarations:
                     if "=" in param:
                         param_delimed = re.split(r'=|\"+', param)
                         param_delimed = list(filter(None, param_delimed))
-                        curr_fxn[PARAM_OPTIONAL_VALS][param_delimed[0]] = param_delimed[1]
-                        curr_fxn[PARAM_NAMES].append(param_delimed[0])
+                        curr_fxn[FunctionAttrs.PARAM_OPTIONAL_VALS.value][param_delimed[0]] = param_delimed[1]
+                        curr_fxn[FunctionAttrs.PARAM_NAMES.value].append(param_delimed[0])
                     else:
-                        curr_fxn[PARAM_NAMES].append(param)
+                        curr_fxn[FunctionAttrs.PARAM_NAMES.value].append(param)
               
             # beginning or end of python doc comment  
             elif stripped_line[0:3] == '"""':
@@ -58,21 +58,21 @@ def process_file(filename):
             # python doc comment
             elif len(stripped_line) > 0 and at_doc_comment:
                 
-                if stripped_line.split(" ")[0] not in curr_fxn[PARAM_NAMES]:
+                if stripped_line.split(" ")[0] not in curr_fxn[FunctionAttrs.PARAM_NAMES.value]:
                     
                     # return desc
                     if stripped_line[0:8] == "Returns:":
                         return_desc_delimed = re.split(r"\[|\]|\s", stripped_line[8:], 4)
                         return_desc_delimed = list(filter(None, return_desc_delimed))
                         
-                        curr_fxn[RETURN_TYPE] = return_desc_delimed[0]
-                        curr_fxn[RETURN_DESC] = return_desc_delimed[1] if len(return_desc_delimed) > 1 else ""
+                        curr_fxn[FunctionAttrs.RETURN_TYPE.value] = return_desc_delimed[0]
+                        curr_fxn[FunctionAttrs.RETURN_DESC.value] = return_desc_delimed[1] if len(return_desc_delimed) > 1 else ""
                         
                     # python doc comment desc
                     else:
-                        if len(curr_fxn[DESC]) > 0:
-                            curr_fxn[DESC] += "\n"
-                        curr_fxn[DESC] += stripped_line
+                        if len(curr_fxn[FunctionAttrs.DESC.value]) > 0:
+                            curr_fxn[FunctionAttrs.DESC.value] += "\n"
+                        curr_fxn[FunctionAttrs.DESC.value] += stripped_line
                         
                 # param desc
                 else:
@@ -83,18 +83,33 @@ def process_file(filename):
                     
                     param_attr = re.split(r"\[|\]", param_desc_delimed[1])
                     param_attr = list(filter(None, param_attr))
-                    if "optional" in param_attr and param_name not in curr_fxn[PARAM_OPTIONAL_VALS].keys():
+                    if "optional" in param_attr and param_name not in curr_fxn[FunctionAttrs.PARAM_OPTIONAL_VALS.value].keys():
                         print("process_file ERROR: Optional parameter not properly defined.", file=sys.stderr)
                     if len(param_attr) > 2:
                         print("process_file ERROR: More param attributes defined than necessary.", file=sys.stderr)
                     
                     param_desc = param_desc_delimed[2]
                     
-                    curr_fxn[PARAM_DESC][param_name] = [param_attr[0], param_desc]
-                
+                    curr_fxn[FunctionAttrs.PARAM_DESC.value][param_name] = [param_attr[0], param_desc]
+                    
+        processed_fxn.append(curr_fxn)
+        
+    return processed_fxn
+
+def generate_html(processed_output):
+    f = open("test.html", "w")
+    
+    f.write(generate_init_html())
+    
+    for fxn in processed_output:
+        f.write(generate_fxn_html(fxn))
+    
+    f.write(generate_end_html())
+    
+    f.close()
 
 def main():
-    process_file("fxn.py")
-
+    processed_output = process_file("fxn.py")
+    generate_html(processed_output)
 
 main()
