fcd7f427c8f200bc35d964b0ff787da6788b468a
Laura Goon <goon.laura@gmail.com>
2022-11-27 19:16:33 -0500

    enums for tracking function attributes

---
+++ enums.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class FunctionAttrs(Enum):
+    NAME = "name"
+    PARAM_NAMES = "param_names"
+    PARAM_OPTIONAL_VALS = "param_optional_vals"
+    DESC = "desc"
+    RETURN_TYPE = "return_type"
+    RETURN_DESC = "return_desc"
+    PARAM_DESC = "param_desc"
